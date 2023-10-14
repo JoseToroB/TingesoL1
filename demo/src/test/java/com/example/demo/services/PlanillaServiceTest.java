c3705c2f02e36ccff6db3edd4c6921edd412f410
Jose Toro <jose.toro.b@usach.cl>
2023-10-14 18:33:26 -0300

    arreglado controlador - service planilla

---
+++ demo/src/test/java/com/example/demo/services/PlanillaServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.services;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+
+public class PlanillaServiceTest {
+}
